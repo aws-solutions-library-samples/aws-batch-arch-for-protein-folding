a7410a9e48deca1ef29a7bb3452deef57e4249be
Brian Loyal <bloyal@amazon.com>
2022-07-07 16:00:05 -0500

    Add AF2 jobs

--- src/batchfold/alphafold2_job.py
+++ src/batchfold/alphafold2_job.py
@@ -4,54 +4,67 @@ from datetime import datetime
 import logging
 
 @define
-class OpenFoldJob(BatchFoldJob):
-    """ Define OpenFold Job """
+class AlphaFold2Job(BatchFoldJob):
+    """ Define AlphaFold 2 Job """
     target_id: str = datetime.now().strftime("%Y%m%d%s")
-    template_mmcif_dir: str = "/database/pdb_mmcif/mmcif_files" 
     fasta_s3_uri: str = ""
     msa_s3_uri: str = ""
-    output_s3_uri: str = ""    
-    use_precomputed_msa: bool = True
-    model_device: str = "cuda:0"
-    config_preset: str = "model_1"
-    jax_param_path: str = ""
-    openfold_checkpoint_path: str = ""
-    save_outputs: bool = False
-    preset: str = "full_dbs"
-    output_postfix: str = ""
-    data_random_seed: str = ""
-    skip_relaxation: bool = False
-    multimer_ri_gap: int = 200
-    output_dir: str = "data/output"
+    output_s3_uri: str = ""
+    data_dir: str = "/data"
+    output_dir: str = "/data/output"
     bfd_database_path: str = "/database/bfd_database_path/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt"
     mgnify_database_path: str = "/database/mgnify_database_path/mgy_clusters_2018_12.fa"
     pdb70_database_path: str = "/database/pdb70_database_path/pdb70"
     obsolete_pdbs_path: str = "/database/obsolete_pdbs_path/obsolete.dat"
+    template_mmcif_dir: str = "/database/template_mmcif_dir/mmcif_files"
     pdb_seqres_database_path: str = "/database/pdb_seqres_database_path/pdb_seqres.txt"
     small_bfd_database_path: str = "/database/small_bfd_database_path/bfd-first_non_consensus_sequences.fasta"
     uniclust30_database_path: str = "/database/uniclust30_database_path/uniclust30_2018_08/uniclust30_2018_08"
     uniprot_database_path: str = "/database/uniprot_database_path/uniprot.fasta"
     uniref90_database_path: str = "/database/uniref90_database_path/uniref90.fasta"
-    max_template_date=datetime.now().strftime("%Y-%m-%d")
+    max_template_date: str = datetime.now().strftime("%Y-%m-%d")
+    db_preset: str = "reduced_dbs"
+    model_preset: str = "monomer"
+    benchmark: bool = False
+    use_precomputed_msa: bool = False
+    features_paths: str = ""
+    run_features_only: bool = False
+    logtostderr: str = True
+    run_relax: bool = True
+    use_gpu_relax: bool = True
+    num_multimer_predictions_per_model: int = 1   
 
     def __attrs_post_init__(self) -> None:
         """Override default BatchFoldJob command"""
 
         download_string = f"aws s3 cp {self.fasta_s3_uri} data/fasta/"    
         if self.use_precomputed_msa:
-            download_string += f" && aws s3 cp --recursive {self.msa_s3_uri} data/msas/{self.target_id}/" 
+            download_string += f" && aws s3 cp --recursive {self.msa_s3_uri} data/{self.target_id}/msas/" 
 
         command_list = [
-            f"python3 /opt/openfold/run_pretrained_openfold.py data/fasta {self.template_mmcif_dir}",
-            f"--model_device={self.model_device}",
-            f"--config_preset={self.config_preset}",
-            f"--multimer_ri_gap={self.multimer_ri_gap}",
-            f"--output_dir {self.output_dir}",
-            "--save_outputs"
+            f"/app/run_alphafold.sh",
+            f"--fasta_paths=/data/fasta/{self.target_id}.fasta",
+            "--data_dir=/database",
+            "--output_dir=/data",
+            "--template_mmcif_dir=/mnt/pdb_mmcif/mmcif_files",
+            "--obsolete_pdbs_path=/mnt/pdb_mmcif/obsolete.dat",
+            # "--uniref90_database_path=/database/uniref90/uniref90.fasta",
+            # "--mgnify_database_path=/mnt/mgnify/mgy_clusters_2018_12.fa",            
+            # "--small_bfd_database_path=/mnt/small_bfd/bfd-first_non_consensus_sequences.fasta",
+            # "--pdb70_database_path=/mnt/pdb70/pdb70",
+            # f"--db_preset={self.db_preset}",
+            f"--max_template_date={self.max_template_date}",
+            f"--model_preset={self.model_preset}",
+            f"--use_precomputed_msas={self.use_precomputed_msa}",
+            f"--benchmark={self.benchmark}",
+            f"--run_relax={self.run_relax}",
+            f"--use_gpu_relax={self.use_gpu_relax}",
+            f"--num_multimer_predictions_per_model={self.num_multimer_predictions_per_model}"
         ]
 
-        if self.use_precomputed_msa is False:
-            command_list.extend([
+        if self.use_precomputed_msa is None:
+            command_list.extend([      
+                f"--db_preset={self.db_preset}",          
                 f"--mgnify_database_path {self.mgnify_database_path}",
                 f"--pdb70_database_path {self.pdb70_database_path}",             
                 f"--uniclust30_database_path /{self.uniclust30_database_path}",
@@ -68,29 +81,11 @@ class OpenFoldJob(BatchFoldJob):
                 ])   
             else:
                 raise ValueError("preset value must be either 'full_dbs' or 'reduced_dbs'")
-        else:
-            command_list.extend(["--use_precomputed_alignments data/msas"])
-            
-        if self.skip_relaxation:
-            command_list.extend(["--skip_relaxation"])
-
-        if self.data_random_seed != "":
-            command_list.extend([f"--data_random_seed={self.data_random_seed}"])
-
-        if self.output_postfix != "":
-            command_list.extend([f"--output_postfix={self.output_postfix}"])
-            
-        if self.openfold_checkpoint_path != "":
-            command_list.extend([f"--openfold_checkpoint_path={self.openfold_checkpoint_path}"])
-        elif self.jax_param_path != "":
-            command_list.extend([f"--jax_param_path={self.jax_param_path}"])
-        else:
-            raise ValueError("Please provide a value for either openfold_checkpoint_path or jax_param_path")
         
         upload_string = f"aws s3 cp --recursive data/output/ {self.output_s3_uri}"
 
         command_string = download_string + " && " + " ".join(command_list) + " && " + upload_string
-        logging.info(f"OpenFold command is \n{command_string}")
+        logging.info(f"AlphaFold2 command is \n{command_string}")
         self.container_overrides["command"] = [command_string]
 
         return None
--- tests/test_alphafold2_job.py
+++ tests/test_alphafold2_job.py
@@ -1,9 +1,8 @@
 from batchfold.batchfold_environment import BatchFoldEnvironment
-from batchfold.openfold_job import OpenFoldJob
+from batchfold.alphafold2_job import AlphaFold2Job
 import pytest
 import boto3
 from datetime import datetime
-
 
 @pytest.fixture()
 def batch_environment():
@@ -11,37 +10,35 @@ def batch_environment():
     assert "BatchEnvironment" in stack.stack_name
     return(stack)
 
-def test_openfold_job_init():
-    new_job = OpenFoldJob(
+def test_alphafold_2_job_init():
+    new_job = AlphaFold2Job(
         job_definition = "Test",
         target_id = "T1084",
         fasta_s3_uri = "s3://aws-af-testing/T1084/fasta/T1084.fasta",
         output_s3_uri = "s3://aws-af-testing/T1084/outputs/",
-        openfold_checkpoint_path = "/database/openfold_params/finetuning_ptm_1.pt",
+        max_template_date = "2022-05-31",
     )
 
     assert new_job.target_id == "T1084"
     assert new_job.fasta_s3_uri == "s3://aws-af-testing/T1084/fasta/T1084.fasta"
     assert new_job.output_s3_uri == "s3://aws-af-testing/T1084/outputs/"
-    assert new_job.template_mmcif_dir == "/database/pdb_mmcif/mmcif_files" 
+    assert new_job.max_template_date == "2022-05-31" 
 
-def test_openfold_job_submission(batch_environment):
+def test_alphafold_2_job_submission(batch_environment):
 
-    job_name = "OpenFoldJob" + datetime.now().strftime("%Y%m%d%s")
+    job_name = "AlphaFoldJ2ob" + datetime.now().strftime("%Y%m%d%s")
     job_queue = batch_environment.queues["G4dnJobQueue"]
-    job_definition = batch_environment.job_definitions["OpenFoldJobDefinition"]
+    job_definition = batch_environment.job_definitions["AlphaFold2JobDefinition"]
 
-    new_job = OpenFoldJob(
+    new_job = AlphaFold2Job(
         job_name = job_name,
         job_definition = job_definition,
         target_id = "T1084",
         fasta_s3_uri = "s3://aws-af-testing/T1084/fasta/T1084.fasta",
-        msa_s3_uri="s3://aws-af-testing/T1084/msas/",
+        msa_s3_uri = "s3://aws-af-testing/T1084/msas/",
         output_s3_uri = "s3://aws-af-testing/T1084/outputs/",
         use_precomputed_msa = True,
-        config_preset = "finetuning_ptm",
-        openfold_checkpoint_path = "/database/openfold_params/finetuning_ptm_1.pt",
-        save_outputs = True        
+        model_preset = "monomer"
     )
 
     response = job_queue.submit_job(new_job)
