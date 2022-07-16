1adbe0253453fad26ab10bbff88a808f8c4815cf
Brian Loyal <bloyal@amazon.com>
2022-07-15 20:16:24 -0500

    Cleanup dockerfiles

--- infrastructure/docker/msa/scripts/download_uniref30.sh
+++ infrastructure/docker/msa/scripts/download_uniref30.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 #
-# Copyright 2021 AlQuraishi Laboratory
+
+# Original Copyright 2022 # Copyright 2021 AlQuraishi Laboratory
+# Modifications Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Downloads and unzips the BFD database for AlphaFold.
-#
-# Usage: bash download_bfd.sh /path/to/download/directory
+# Usage: bash download_uniref30.sh /path/to/download/directory
 set -e
 
 if [[ $# -eq 0 ]]; then
