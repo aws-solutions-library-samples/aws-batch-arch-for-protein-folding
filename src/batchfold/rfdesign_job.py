8c221b5e0137889a6ff50426e58947fe0928b310
Brian Loyal <bloyal@amazon.com>
2022-11-17 17:06:03 -0600

    Fix merge issues

--- src/batchfold/rfdesign_job.py
+++ src/batchfold/rfdesign_job.py
@@ -14,7 +14,6 @@ class RFDesignHallucinateJob(BatchFoldJob):
     input_s3_uri: str
     output_s3_uri: str 
     pdb: str    
-    output_dir: str = uuid.uuid4().hex
     job_definition_name: str = "RFDesignJobDefinition"
     weights_dir: str = "/database/rfdesign_params/hallucination"
     params: dict = {}
@@ -27,12 +26,12 @@ class RFDesignHallucinateJob(BatchFoldJob):
     def __attrs_post_init__(self) -> None:
         """Override default BatchFoldJob command"""
 
-        command_list = [f"-i {self.input_s3_uri}:{self.output_dir}/input/"]
-        command_list.extend([f"-o {self.output_dir}/output/:{self.output_s3_uri}/"])
+        command_list = [f"-i {self.input_s3_uri}:input/"]
+        command_list.extend([f"-o output/:{self.output_s3_uri}"])
         command_list.extend([
             "python hallucination/hallucinate.py",
-            f"--pdb={self.output_dir}/input/{self.pdb}",
-            f"--out={self.output_dir}/predictions/output",
+            f"--pdb={self.pdb}",
+            f"--out=output/output",
             f"--weights_dir={self.weights_dir}" 
         ])
         command_list.extend([f"--{key}={value}" for key, value in self.params.items()])
@@ -47,7 +46,6 @@ class RFDesignInpaintJob(BatchFoldJob):
     input_s3_uri: str
     output_s3_uri: str 
     pdb: str    
-    output_dir: str = uuid.uuid4().hex
     job_definition_name: str = "RFDesignJobDefinition"
     checkpoint: str = "/database/rfdesign_params/inpainting/BFF_mix_epoch25.pt"
     params: dict = {}
@@ -60,15 +58,20 @@ class RFDesignInpaintJob(BatchFoldJob):
     def __attrs_post_init__(self) -> None:
         """Override default BatchFoldJob command"""
 
-        command_list = [f"-i {self.input_s3_uri}:{self.output_dir}/input/"]
-        command_list.extend([f"-o {self.output_dir}/output/:{self.output_s3_uri}"])
+        command_list = [f"-i {self.input_s3_uri}:input/"]
+        command_list.extend([f"-o output/:{self.output_s3_uri}"])
         command_list.extend([
             "python inpainting/inpaint.py",
-            f"--pdb={self.output_dir}/input/{self.pdb}",
-            f"--out={self.output_dir}/predictions/output",
-            f"--checkpoint={self.weights_dir}" 
+            f"--pdb={self.pdb}",
+            f"--out=output/output",
+            f"--checkpoint={self.checkpoint}" 
         ])
-        command_list.extend([f"--{key}={value}" for key, value in self.params.items()])
+
+        for key, value in self.params.items():
+            if key in ['verbose', 'out', 'dump_pdb', 'dump_trb', 'dump_npz', 'dump_fasta', 'dump_all']:
+                command_list.extend([f"--{key}"])
+            else:
+                command_list.extend([f"--{key}={value}"])
         logging.info(f"Command is \n{command_list}")
         self.define_container_overrides(command_list, self.cpu, self.memory, self.gpu)
         return None        
