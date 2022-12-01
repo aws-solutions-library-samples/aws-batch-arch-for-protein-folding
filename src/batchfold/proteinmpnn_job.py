26137c5c0f5d868bb7fd681f6de451eb8815c260
Brian Loyal <bloyal@amazon.com>
2022-12-01 12:45:21 -0600

    fix jsonline updates

--- src/batchfold/proteinmpnn_job.py
+++ src/batchfold/proteinmpnn_job.py
@@ -5,6 +5,8 @@ from attrs import define, field
 from batchfold.batchfold_job import BatchFoldJob
 from datetime import datetime
 import logging
+import os
+
 
 @define
 class ProteinMPNNJob(BatchFoldJob):
@@ -14,12 +16,19 @@ class ProteinMPNNJob(BatchFoldJob):
     jsonl_s3_uri: str = ""
     output_s3_uri: str = ""
     job_definition_name: str = "ProteinMPNNJobDefinition"
-    target_id: str = datetime.now().strftime("%Y%m%d%s")
     job_name: str = field(default="ProteinMPNNJob" + datetime.now().strftime("%Y%m%d%s"))
     job_definition_name: str = field(default="ProteinMPNNJobDefinition")
     cpu: int = field(default=4)
     memory: int = field(default=15)
     gpu: int = field(default=0)
+
+    chain_id_jsonl: str = field(default="")
+    fixed_positions_jsonl: str = field(default="")
+    bias_AA_jsonl: str = field(default="")
+    bias_by_res_jsonl: str = field(default="")
+    omit_AA_jsonl: str = field(default="")
+    pssm_jsonl: str = field(default="")
+    tied_positions_jsonl: str = field(default="")
 
     ca_only: bool = field(default=False)
     path_to_model_weights: str = field(default="")
@@ -37,42 +46,65 @@ class ProteinMPNNJob(BatchFoldJob):
     max_length: int = field(default=200000)
     sampling_temp: str = field(default="0.1")
     pdb_path_chains: str = field(default="")
-    chain_id_jsonl: str = field(default="")
-    fixed_positions_jsonl: str = field(default="")
+    suppress_print: int = field(default=0)    
     omit_AAs: str = field(default="X")
-    bias_AA_jsonl: str = field(default="")
-    bias_by_res_jsonl: str = field(default="")
-    omit_AA_jsonl: str = field(default="")
-    pssm_jsonl: str = field(default="")
     pssm_multi: float = field(default=0.00)
     pssm_threshold: float = field(default=0.00)
     pssm_log_odds_flag: int = field(default=0)
     pssm_bias_flag: int = field(default=0)
-    tied_positions_jsonl: str = field(default="")
 
     def __attrs_post_init__(self) -> None:
         """Override default BatchFoldJob command"""
 
         if self.pdb_s3_uri:
-            input_command = f"-i {self.pdb_s3_uri}:input/{self.target_id}.pdb"
+            input_command = f"-i {self.pdb_s3_uri}:input/{os.path.basename(self.pdb_s3_uri)}"
         elif self.jsonl_s3_uri:
-            input_command = f"-i {self.jsonl_s3_uri}:input/{self.target_id}.jsonl"
+            input_command = f"-i {self.jsonl_s3_uri}:input/input.jsonl"
         else:
             raise ValueError(
-                "Please specify a value for either pdb_s3_uri or pdb_s3_uri"
+                "Please specify a value for either pdb_s3_uri or jsonl_s3_uri"
             )
 
         command_list = [input_command]
+
+        if self.chain_id_jsonl:
+            command_list.extend([f"-i {self.chain_id_jsonl}:input/chain_id.jsonl"])
+        if self.fixed_positions_jsonl:
+            command_list.extend([f"-i {self.fixed_positions_jsonl}:input/fixed_positions.jsonl"])
+        if self.bias_AA_jsonl:
+            command_list.extend([f"-i {self.bias_AA_jsonl}:input/bias_AA.jsonl"])
+        if self.bias_by_res_jsonl:
+            command_list.extend([f"-i {self.bias_by_res_jsonl}:input/bias_by_res.jsonl"])
+        if self.omit_AA_jsonl:
+            command_list.extend([f"-i {self.omit_AA_jsonl}:input/omit_AA.jsonl"])
+        if self.pssm_jsonl:
+            command_list.extend([f"-i {self.pssm_jsonl}:input/pssm.jsonl"])
+        if self.tied_positions_jsonl:
+            command_list.extend([f"-i {self.tied_positions_jsonl}:input/tied_positions.jsonl"])
+
         command_list.extend([f"-o output/:{self.output_s3_uri}"])
         command_list.extend(["python protein_mpnn_run.py"])
         command_list.extend([f"--out_folder=output"])
         
         if self.pdb_s3_uri:
-            command_list.extend([f"--pdb_path=input/{self.target_id}.pdb"])
+            command_list.extend([f"--pdb_path=input/{os.path.basename(self.pdb_s3_uri)}"])
             command_list.extend([f"--pdb_path_chains='{self.pdb_path_chains}'"])
-
         elif self.jsonl_s3_uri:
-            command_list.extend([f"--jsonl_s3_uri=input/{self.target_id}.jsonl"])
+            command_list.extend([f"--jsonl_s3_uri=input/input.jsonl"])
+        if self.chain_id_jsonl:
+            command_list.extend([f"--chain_id_jsonl=input/chain_id.jsonl"])    
+        if self.fixed_positions_jsonl:
+            command_list.extend([f"--fixed_positions_jsonl=input/fixed_positions.jsonl"])
+        if self.bias_AA_jsonl:
+            command_list.extend([f"--bias_AA_jsonl=input/bias_AA.jsonl"])            
+        if self.bias_by_res_jsonl:
+            command_list.extend([f"--bias_by_res_jsonl=input/bias_by_res.jsonl"])
+        if self.omit_AA_jsonl:
+            command_list.extend([f"--omit_AA_jsonl=input/omit_AA.jsonl"])
+        if self.pssm_jsonl:
+            command_list.extend([f"--pssm_jsonl=input/pssm.jsonl"])
+        if self.tied_positions_jsonl:
+            command_list.extend([f"--tied_positions_jsonl=input/tied_positions.jsonl"])
 
         if self.ca_only:
             command_list.extend([f"--ca_only={self.ca_only}"])
@@ -104,20 +136,8 @@ class ProteinMPNNJob(BatchFoldJob):
             command_list.extend([f"--max_length={self.max_length}"])
         if self.sampling_temp:
             command_list.extend([f"--sampling_temp={self.sampling_temp}"])
-        if self.chain_id_jsonl:
-            command_list.extend([f"--chain_id_jsonl={self.chain_id_jsonl}"])
-        if self.fixed_positions_jsonl:
-            command_list.extend([f"--fixed_positions_jsonl={self.fixed_positions_jsonl}"])
         if self.omit_AAs:
-            command_list.extend([f"--omit_AAs={self.omit_AAs}"])
-        if self.bias_AA_jsonl:
-            command_list.extend([f"--bias_AA_jsonl={self.bias_AA_jsonl}"])
-        if self.bias_by_res_jsonl:
-            command_list.extend([f"--bias_by_res_jsonl={self.bias_by_res_jsonl}"])
-        if self.omit_AA_jsonl:
-            command_list.extend([f"--omit_AA_jsonl={self.omit_AA_jsonl}"])
-        if self.pssm_jsonl:
-            command_list.extend([f"--pssm_jsonl={self.pssm_jsonl}"])
+            command_list.extend([f"--omit_AAs={self.omit_AAs}"])    
         if self.pssm_multi:
             command_list.extend([f"--pssm_multi={self.pssm_multi}"])
         if self.pssm_threshold:
@@ -125,9 +145,9 @@ class ProteinMPNNJob(BatchFoldJob):
         if self.pssm_log_odds_flag:
             command_list.extend([f"--pssm_log_odds_flag={self.pssm_log_odds_flag}"])
         if self.pssm_bias_flag:
-            command_list.extend([f"--pssm_bias_flag={self.pssm_bias_flag}"])
-        if self.tied_positions_jsonl:
-            command_list.extend([f"--tied_positions_jsonl={self.tied_positions_jsonl}"])      
+            command_list.extend([f"--pssm_bias_flag={self.pssm_bias_flag}"])    
+        if self.suppress_print:
+            command_list.extend([f"--suppress_print={self.suppress_print}"])                  
         
         logging.info(f"Command is \n{command_list}")
         self.define_container_overrides(command_list, self.cpu, self.memory, self.gpu)
